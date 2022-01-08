5eeacd721227add6004d8ba1cfc81244a0d0b687
KDOXG <kevin.20pereira@gmail.com>
2022-01-08 08:27:11 -0300

    testes com classes, rodando C# pela 1a vez

---
+++ Aulas1/Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposEVariaveis
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int numero1 = 10;
+			int numero2 = 20;
+			var soma = numero1 + numero2;
+			Console.WriteLine("O valor da conta eh " + soma);
+
+			var quadrado1 = new Quadrado();
+			quadrado1.lado = 10;
+			var quadrado2 = quadrado1;
+			quadrado2.lado = 11;
+			Console.WriteLine(quadrado1.lado);
+			Console.WriteLine(quadrado2.lado);
+		}
+		class Quadrado
+        {
+			public int lado;
+        }
+	}
+
+}
